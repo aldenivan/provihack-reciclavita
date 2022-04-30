d3bef36f280596ed9144dc856066d25ad78e1bf4
Roberto Bressanelli <rbressanelli@gmail.com>
2022-04-30 15:13:33 -0300

    Feat: changed some header style

--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -4,17 +4,24 @@ export const Navbar = styled.nav`
   width: 100%;
   min-width: 100%;
   background-color: #052d53;
-  height: 60px;
+  /* height: 60px; */
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
-  padding: 0 10px;
+  padding: 10px 10px;
 
   font-family: "Lato";
   font-weight: 700;
   font-size: 16px;
-  line-height: 19px;
+  /* line-height: 19px; */
   color: white;
+
+  @media only screen and (min-width: 768px) {
+    line-height: 19px;
+    height: 60px;
+    padding: 0 10px;
+    justify-content: space-around;
+  }
 
   img {
     width: 150px;
@@ -23,13 +30,21 @@ export const Navbar = styled.nav`
   ul {
     display: flex;
     justify-content: space-between;
-    align-items: center;
+    flex-direction: column;
+    align-items: left;
     width: 45%;
+    list-style: none;
 
     a {
       text-decoration: none;
       color: white;
       font-size: 18px;
+    }
+
+    @media only screen and (min-width: 768px) {
+      flex-direction: row;
+      /* justify-content: space-around; */
+      align-items: center;
     }
   }
 `;
