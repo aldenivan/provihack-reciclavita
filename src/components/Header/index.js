cb11440219e046f836dc0a092268f0f518121efa
Aldenivan Jr <aldenivan1207@gmail.com>
2022-04-30 12:29:11 -0300

    feat: creating header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,23 @@
-import {Link} from 'react-router-dom'
+import LogoHeader from "../../assets/image/logo.png";
+import { Navbar } from "./styled";
+
+import { Link } from "react-router-dom";
 const Header = () => {
-    return(
-        <header>
-            <nav>
-                <Link to='/'> Home </Link>
-                <Link to='/Information'>Informativo</Link>
-                <Link to='/Dashboard'> Postos de Coleta </Link>
-            </nav>
-        </header>
-    )
-}
-export default Header
+  return (
+    <header>
+      <Navbar>
+        <Link to="/">
+          {" "}
+          <img src={LogoHeader} alt="logo_header" />{" "}
+        </Link>
+        <ul>
+          <Link to="/"> Home </Link>
+          <Link to="/Information">Informativo</Link>
+          <Link to="/Dashboard"> Postos de Coleta </Link>
+          <Link to="/About Us"> Sobre Nós </Link>
+        </ul>
+      </Navbar>
+    </header>
+  );
+};
+export default Header;
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,13 +1,11 @@
-import Header from "../../components/Header"
-import Button from "../../components/Button/button"
+import Header from "../../components/Header";
+// import Button from "../../components/Button/button";
 const HomePage = () => {
-    return (
-        <>
-        <Header/>
-            <h1>Home Page</h1>
-            
-        </>
-        
-    )
-}
-export default HomePage
+  return (
+    <>
+      <Header />
+      <h1>Home Page</h1>
+    </>
+  );
+};
+export default HomePage;
